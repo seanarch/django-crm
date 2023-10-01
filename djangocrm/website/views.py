451a09455ee781ba63d2590e68ebc11f2bc96609
seanarch <zhuqingyun@outlook.com>
2023-10-01 17:23:44 -0600

    refactor home.html

--- djangocrm/website/views.py
+++ djangocrm/website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages 
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request): 
+    records = Record.objects.all() 
+
     # check to see if logging in 
     if request.method == "POST": 
         username = request.POST['username']
@@ -19,7 +22,7 @@ def home(request):
             messages.success(request, "There was an error when logging in. Please try again.")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 def login_user(request):
     pass 
