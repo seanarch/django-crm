3cdcc9b830e4ffa4efeea171a6ea631e7938d9c4
seanarch <zhuqingyun@outlook.com>
2023-10-01 00:05:41 -0600

    Add login and login error

--- djangocrm/website/views.py
+++ djangocrm/website/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages 
 
 # Create your views here.
 def home(request): 
-    return render(request, 'home.html', {})
+    # check to see if logging in 
+    if request.method == "POST": 
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate 
+        user = authenticate(request, username = username, password = password) 
+        if user is not None: 
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!") 
+            return redirect('home')
+        else: 
+            messages.success(request, "There was an error when logging in. Please try again.")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
 
 def login_user(request):
     pass 
